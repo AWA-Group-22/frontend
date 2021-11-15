3abc4e5a4522a342dad5e8b7f2326da5dd6830e0
Alenkaara <78751694+Alenkaara@users.noreply.github.com>
2021-11-15 12:45:23 +0200

    Advancing the project
    Added more page.js files and css modules in the components section.

--- gateway-takeaway-frontend/src/App.js
+++ gateway-takeaway-frontend/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import LoginPage from './components/LoginPage';
+import SignUpPage from './components/SignUpPage';
+import ShoppingCart from './components/ShoppingCart'
+import ContactList from './components/ContactList'
+import ContactDetailView from './components/ContactDetailView';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import ContactData from './data.json';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+
+  const contacts = ContactData.map(contact => {
+    return { ...contact, id: uuidv4() }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="headerBackground">
+        <div className="headerContainer">
+          <Link to="/"><div className="brandText">Gateway Takeaway</div></Link>
+          <Link to="/searchfield"><div>Search__________________</div></Link>
+          <Link to="/loginpage"><div>Login</div></Link>
+          <Link to="/signup-page"><div>Create account</div></Link>
+          <Link to="/contacts"><div className="orderText">Orders</div></Link>
+        </div>
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/shoppingcart" element={ <ShoppingCart /> } />
+          <Route path="/contacts" element={ <ContactList contacts={ contacts }/> } >
+            <Route path=":contactId" element={ <ContactDetailView contacts={ contacts } /> } />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ gateway-takeaway-frontend/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Home() {
+  return (
+    <div>
+      <h1></h1>
+      <p>
+      </p>
+      <Link to="ShoppingCart"><button>Shopping cart</button></Link>
+    </div>
+  )
+}
