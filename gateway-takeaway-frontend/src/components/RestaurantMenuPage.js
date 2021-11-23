e077748efdd9bfe204e52a2e080d1da67bd7fe29
Alenkaara <78751694+Alenkaara@users.noreply.github.com>
2021-11-23 13:02:26 +0200

    More code
    Added code to restaurant menu and others

--- gateway-takeaway-frontend/src/components/RestaurantMenuPage.js
+++ gateway-takeaway-frontend/src/components/RestaurantMenuPage.js
@@ -11,12 +11,19 @@ export default class RestaurantMenuPage extends Component {
                 <div className={ styles.deliveryText }>Delivery in 15-25 minutes to #sample address#</div>
                 <div className={ styles.hourText }>Open today: #sample time#</div>
                 <div className={ styles.backButton }>
-                    <Link to="/home" style={{ color: 'inherit', textDecoration: 'none' }}>Back</Link></div>
+                    <Link to="/" style={{ color: 'inherit', textDecoration: 'none' }}>Back</Link></div>
                 <div className={ styles.restaurantTitle }>Example restaurant Oulu</div>
                 <div className={ styles.restaurantMotto }>Example motto</div>
-                <div className={ styles.deliveryCost }>DELIVERY: sample</div>
-                <div className={ styles.minOrder }>MIN. ORDER: sample</div>
-                
+                <div className={ styles.deliveryCost }>DELIVERY: 7.90</div>
+                <div className={ styles.minOrder }>MIN. ORDER: 20</div>
+                <div className={ styles.subContainer }>🙂 8.9</div>
+                <div className={ styles.outText }>out of 10</div>
+                <div className={ styles.foodCategory1 }>Category1</div>
+                <div className={ styles.foodCategory2 }>Category2</div>
+                <div className={ styles.foodCategory3 }>Category3</div>
+                <div className={ styles.foodCategoryContainer1 }>asdasd</div>
+                <div className={ styles.foodCategoryContainer2 }>asdasd</div>
+                <div className={ styles.foodCategoryContainer3 }>asdasd</div>
             </div>
         )
     }
