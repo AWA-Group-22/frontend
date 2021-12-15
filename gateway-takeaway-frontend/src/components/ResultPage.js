5c5a8ab359d1f3f1951f23f3be9d785eaeb3d18d
Alenkaara <78751694+Alenkaara@users.noreply.github.com>
2021-12-15 09:07:51 +0200

    Update ResultPage.js

--- gateway-takeaway-frontend/src/components/ResultPage.js
+++ gateway-takeaway-frontend/src/components/ResultPage.js
@@ -48,22 +48,6 @@ export default function ResultPage() {
           console.error(error);
         }
       };
-    
-    // const postSearchRestaurant = () => {
-    //     axios({
-    //         headers: {
-    //           'Content-Type': 'application/json',
-    //           'Authorization': 'Bearer ' + UserAuthContextValue.jwt
-    //         },
-    //         method: "post",
-    //         data: {
-    //             restaurant_name: restaurants,
-    //         },
-    //         url: "https://back-end-22-group.herokuapp.com/restaurant/search",
-    //     })
-    //     .then((res) => console.log(res));
-    // };
-    
 
     return (
         <div>
@@ -80,9 +64,6 @@ export default function ResultPage() {
                 <input ref={textInput} type="text" />  
                 <button onClick={searchRestaurant}>Search</button>  
             </div>
-            {/* <input placeholder="type a restaurant" onClick={e => setRestaurants(e.target.value)} /> */}
-            {/* <button onClick={postSearchRestaurant}> Search </button>
-            <button >  </button> */}
         </div>
 
         {
@@ -99,35 +80,6 @@ export default function ResultPage() {
             </div>;
             }) : "No data yet!"
         }
-
-        {/* <div className={ styles.restaurantContainer }>
-                    {restaurants.length ? restaurants : <div> No restaurant yet </div>}
-                    <img src={restaurants.image} width={336} height={180} />
-                    <div> {restaurants.restaurant_name} </div>
-                    <div> {restaurants.address} </div>
-                    <div> {restaurants.operating_hours} </div>
-                    <div> {restaurants.type} </div>
-                    <div> {restaurants.price_level} </div>
-                </div>
-
-        {
-            restaurants.map((restaurant) => {
-                return <div className={ styles.restaurantContainer }>
-                    {/* {restaurant.length ? restaurant : <div> No restaurant yet </div>}
-                    <img src={restaurant.image} width={336} height={180} />
-                    <div> {restaurant.restaurant_name} </div>
-                    <div> {restaurant.address} </div>
-                    <div> {restaurant.operating_hours} </div>
-                    <div> {restaurant.type} </div>
-                    <div> {restaurant.price_level} </div>
-                </div>
-            })
-        }
-        return(
-        <div className='list'>
-            {notes.length ? notes : <p>Default Markup</p>}
-        </div>
-        ); */}
         </div>
     )
 }
